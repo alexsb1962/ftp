726973e325bd6e1e19f8fdb1f0679459cd0d69be
alex <alexsb@mail.ru>
2019-10-03 16:01:25 +0300

    Лечить пробелы в имени файла

--- ftp2img2ftp.py
+++ ftp2img2ftp.py
@@ -1,33 +1,60 @@
 
-
+import datetime as dt
+import os
 import ftplib
 
 print('Start')
 
-def func(s):
-    l=s.rsplit()
-    d.append( l[-1] )
 
-def around(f,folder=''):
-    if folder=='':
-        f.cwd('Furry_Archive/Logos')
-    d = []
-    f.dir(lambda s:d.append(s.split()[-1]))
+def around(f):
+    start_path=os.getcwd()
 
-    for entry in d:
-        if entry=='.': continue
-        if entry=='..': continue
-        ext=entry.split('.')[-1]
-        if ext=='jpg' or ext=='gif':
-            # загрузка
-            with open(entry, 'wb') as fp:
-                f.retrbinary('RETR '+entry, fp.write)
+    dirlist = []
+    f.dir(lambda s: dirlist.append(s))
+
+    for s in dirlist:
+        #todo имена файлов могут содержать пробелы
+        words=s.split() # начиная с девятого элемента выдачи list
+        fname=''
+        for w in words:
+            fname=fname+w
+
+        if fname == '.': continue
+        if fname == '..': continue
+        if s[0]=='d':
+            print('Переход в ',fname,' с рекурсивным вызовом')
+            f.cwd(fname)
+            try:
+                os.mkdir(fname)
+            except FileExistsError:
+                pass
+            os.chdir(fname)
+            around(f)
+            os.chdir('..')
+            f.cwd('..')
+        else:
+            ext = fname.split('.')[-1]
+            if ext == 'jpg' or ext == 'gif':
+                # загрузка
+                with open(fname, 'wb') as fp:
+                    try:
+                        f.retrbinary('RETR ' + fname, fp.write)
+                    except:
+                        print('Не возможно принять файл '+fname)
+                        print(s)
 
 with ftplib.FTP(host='home.dimonius.ru',user='furry',passwd='letsgo' ) as f:
-    #создать папку
-    around(f,'')
+    # корневой каталог (начало работы)
+    day=dt.datetime.today()
+    dirname=str(day.day)+'_'+str(day.month)+'_'+str(day.year)
+    dirname=dirname+'T'+str(day.hour)+'_'+str(day.minute)+'_'+str(day.second)+'_'+str(day.microsecond)
+    try:
+        os.mkdir(dirname)
+    except  FileExistsError:
+        pass
+    os.chdir(dirname)
+    around(f)
 
-
-print(d)
+print('The END')
 
 
