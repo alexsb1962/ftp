e814d03cc739b142db399c1bcfd8952c9fc3c64e
alex <alexsb@mail.ru>
2019-10-09 02:57:39 +0300

    поработаем с вн. программами

--- ftp2img2ftp.py
+++ ftp2img2ftp.py
@@ -7,29 +7,30 @@ import ftplib
 
 print('Start')
 
-class ftpplus( ftplib.FTP):
+
+class ftpplus(ftplib.FTP):
     pass
 
 
-def around(f,recursive=False,mustload=False):
+def around(f, recursive=False, mustload=False):
     """
      Входной параметр - открытое ftp подключение
     :rtype: None
     """
-    start_path=os.getcwd()
+    start_path = os.getcwd()
 
     for elem in f.mlsd():
 
-        fname,dict=elem
+        fname, dict = elem
 
         if dict['type'] == 'pdir': continue
         if dict['type'] == 'cdir': continue
-        if dict['type']=='dir':
-            print('Переход в ',start_path+'\\'+fname)
+        if dict['type'] == 'dir':
+            print('Переход в ', start_path + '\\' + fname)
             try:
                 f.cwd(fname)
             except:
-                print('Безуспешный переход ',start_path,'\\',fname)
+                print('Безуспешный переход ', start_path, '\\', fname)
                 exit(0)
             try:
                 os.mkdir(fname)
@@ -37,10 +38,10 @@ def around(f,recursive=False,mustload=False):
                 pass
             if recursive:
                 os.chdir(fname)
-                around(f,recursive)
+                around(f, recursive)
                 os.chdir('..')
                 f.cwd('..')
-        if dict['type']=='file':
+        if dict['type'] == 'file':
             ext = fname.split('.')[-1]
             if ext == 'jpg' or ext == 'gif':
                 # загрузка
@@ -52,33 +53,33 @@ def around(f,recursive=False,mustload=False):
                         else:
                             print('Имитация загрузки ', start_path + '\\' + fname)
                     except:
-                        print('Не возможно принять файл '+fname)
+                        print('Не возможно принять файл ' + fname)
                         print(s)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
-    parser=argparse.ArgumentParser()
-    parser.add_argument('-url',default='home.dimonius.ru')
-    parser.add_argument('-l',  default='')
-    parser.add_argument('-d',  default='')
-    parser.add_argument('-u',  default='furry')
-    parser.add_argument('-p',  default='letsgo')
-    names=parser.parse_args()
-    user_name=names.u
-    url=names.url
-    password=names.p
-    local=names.l
-    nlocal=names.d
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-url', default='home.dimonius.ru')
+    parser.add_argument('-l', default='')
+    parser.add_argument('-d', default='')
+    parser.add_argument('-u', default='furry')
+    parser.add_argument('-p', default='letsgo')
+    names = parser.parse_args()
+    user_name = names.u
+    url = names.url
+    password = names.p
+    local = names.l
+    nlocal = names.d
 
     try:
-        f=ftpplus()
+        f = ftpplus()
         f.encoding = 'utf-8'
         f.connect(host=url)
-        f.login(user=user_name,passwd=password)
+        f.login(user=user_name, passwd=password)
 
-        response=f.sendcmd('TYPE A')
-        response=f.sendcmd('FEAT')
+        response = f.sendcmd('TYPE A')
+        response = f.sendcmd('FEAT')
         if not 'UTF8' in response:
             print('Не найдена поддержка UTF8')
             exit(0)
@@ -89,30 +90,28 @@ if __name__=="__main__":
 
         f.set_pasv(True)
 
-
-
         # корневой каталог (начало работы)
         if nlocal:
             try:
                 f.cwd(nlocal)
             except:
-                print('Не удалось перейти на ',nlocal)
+                print('Не удалось перейти на ', nlocal)
             finally:
                 exit(0)
         if local:
-            dirname=local
+            dirname = local
         else:
-           day=dt.datetime.today()
-           dirname=str(day.year)+'_'+str(day.month)+'_'+str(day.day)
-           dirname=dirname+'_T'+str(day.hour)+'_'+str(day.minute)+'_'+str(day.second)+'_'+str(day.microsecond)
+            day = dt.datetime.today()
+            dirname = str(day.year) + '_' + str(day.month) + '_' + str(day.day)
+            dirname = dirname + '_T' + str(day.hour) + '_' + str(day.minute) + '_' + str(day.second) + '_' + str(
+                day.microsecond)
         try:
             os.mkdir(dirname)
         except  FileExistsError:
             pass
         os.chdir(dirname)
 
-
-        around(f,recursive=True,mustload=True)
+        around(f, recursive=True, mustload=True)
 
     finally:
         pass
